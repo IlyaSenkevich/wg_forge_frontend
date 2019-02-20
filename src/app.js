9f175ecf09aee1eb00620fd8e8ede0e270a50e61
Ilya Senkevich <ilya.senkevich.19@gmail.com>
2019-02-21 01:34:37 +0300

    cardNumber

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 // this is an example of improting data from JSON
-import 'orders' from '../data/orders.json';
+import orders from '../data/orders.json';
+
+const cardNumber = (str) => {
+   const newStr = str.replace(/\d(?=\d{4})/g, "*");
+   return newStr;
+};
+
+const dateFormat = (date) => {
+ const newDate = date 
+};
 
 export default (function () {
-    // YOUR CODE GOES HERE
-    // next line is for example only
-    document.getElementById("app").innerHTML = "<h1>Hello WG Forge</h1>";
+    orders.map(order => {
+      const str = cardNumber(order.card_number);
+        document.getElementById("app").innerHTML += `
+        <tr id=${order.id}>
+            <td>${order.transaction_id}</td>
+            <td class="user_data">${order.user_id}</td>
+            <td>${order.created_at}</td>
+            <td>${order.total}</td>
+            <td>${str}</td>
+            <td>${order.order_country}</td>
+            <td>${order.order_ip}<td>
+        </tr>`});
 }());
